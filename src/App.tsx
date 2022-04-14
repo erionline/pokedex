8c498c93c24d4d820c1c76db22c2f9ca0f8ec180
Eray C <hi@eri.gg>
2022-04-14 12:55:15 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const [page, setPage] = useState({
+  current: () => Home,
+});
+
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div 
+      className="flex flex-col items-start min-h-screen p-10 rounded"
+      style={{
+        backgroundImage: "url(https://cdn.eri.gg/2ppB9UtjJiO1jKE6CwcP/download)",
+        backgroundSize: "cover",
+      }}
+    >
+      <h1 
+        className="mt-24 text-6xl tracking-widest title text-poke-yellow"
+      >
+        My Pokédex
+      </h1>
+      <p className="mt-5 text-lg text-center text-poke-darkblue">
+        Votre pokédex personnel entièrement personnalisable.
+      </p>
+      <div className="grid grid-cols-2 gap-4 mt-5 text-white">
+        <button 
+          className="px-4 py-1 font-bold rounded bg-poke-darkblue"
+          onClick={() => setPage({ current: () => List })}
         >
-          Learn React
-        </a>
-      </header>
+          Consulter la liste
+        </button>
+        <button className="px-4 font-bold rounded bg-poke-darkblue">
+          Ajouter un pokémon
+        </button>
+      </div>
     </div>
   );
+}
+
+const List = () => {
+  return (
+    <div 
+      className="flex flex-col items-start min-h-screen p-10 rounded"
+      style={{
+        backgroundImage: "url(https://cdn.eri.gg/2ppB9UtjJiO1jKE6CwcP/download)",
+        backgroundSize: "cover",
+      }}
+    >
+    <h1 
+      className="mt-24 text-6xl tracking-widest text-poke-yellow"
+    >
+      Liste des pokémons
+    </h1>
+    <p className="mt-5 text-lg text-center text-poke-darkblue">
+      Votre pokédex personnel entièrement personnalisable.
+    </p>
+  </div>
+  );
+}
+
+const App = () => {
+  return <page.current />;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "poke-blue": "#0075BE",
+        "poke-darkblue": "#0A285F",
+        "poke-lightblue": "#5EBDFC",
+        "poke-lighterblue": "#16D6FB",
+        "poke-yellow": "#FFCC00",
+        "poke-darkyellow": "#D5A100",
+        "poke-red": "#FB1B1B",
+      },
+    },
+  },
+  plugins: [],
+}
