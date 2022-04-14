4a288f4dd65712dd93b6e2639d7f38529ea77f56
Bafle <Bafle31@gmail.com>
2022-04-15 00:00:11 +0200

    Added Router and begin dev of list

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react';
-import './App.css';
+import { render } from "@testing-library/react";
+import { Component, useEffect } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-const [page, setPage] = useState({
-  current: () => Home,
-});
+/*
+state = {
+  name: "",
+  imageUrl: "",
+  pokemonIndex: "",
+};
 
-const Home = () => {
+useEffect {
+  const { name, url } = this.props;
+  const pokemonIndex = url.split("/")[url.split("/").length - 2];
+  const imageUrl = `http://pokeapi.co/media/sprites/pokemon/${pokemonIndex}.png`;
+  this.setState({ name, imageUrl, pokemonIndex });
+}
+*/
+
+export default function App() {
   return (
-    <div 
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/list" element={<List />} />
+    </Routes>
+  );
+}
+
+export function Home() {
+  return (
+    <div
       className="flex flex-col items-start min-h-screen p-10 rounded"
       style={{
-        backgroundImage: "url(https://cdn.eri.gg/2ppB9UtjJiO1jKE6CwcP/download)",
+        backgroundImage:
+          "url(https://cdn.eri.gg/2ppB9UtjJiO1jKE6CwcP/download)",
         backgroundSize: "cover",
       }}
     >
-      <h1 
-        className="mt-24 text-6xl tracking-widest title text-poke-yellow"
-      >
+      <h1 className="mt-24 text-6xl tracking-widest title text-poke-yellow">
         My Pokédex
       </h1>
       <p className="mt-5 text-lg text-center text-poke-darkblue">
         Votre pokédex personnel entièrement personnalisable.
       </p>
       <div className="grid grid-cols-2 gap-4 mt-5 text-white">
-        <button 
-          className="px-4 py-1 font-bold rounded bg-poke-darkblue"
-          onClick={() => setPage({ current: () => List })}
-        >
-          Consulter la liste
-        </button>
+        <Link to="/list">
+          <button className="px-4 py-1 font-bold rounded bg-poke-darkblue">
+            Consulter la liste
+          </button>
+        </Link>
         <button className="px-4 font-bold rounded bg-poke-darkblue">
           Ajouter un pokémon
         </button>
@@ -37,29 +57,22 @@ const Home = () => {
   );
 }
 
-const List = () => {
+export function List() {
   return (
-    <div 
+    <div
       className="flex flex-col items-start min-h-screen p-10 rounded"
       style={{
-        backgroundImage: "url(https://cdn.eri.gg/2ppB9UtjJiO1jKE6CwcP/download)",
+        backgroundImage:
+          "url(https://cdn.eri.gg/2ppB9UtjJiO1jKE6CwcP/download)",
         backgroundSize: "cover",
       }}
     >
-    <h1 
-      className="mt-24 text-6xl tracking-widest text-poke-yellow"
-    >
-      Liste des pokémons
-    </h1>
-    <p className="mt-5 text-lg text-center text-poke-darkblue">
-      Votre pokédex personnel entièrement personnalisable.
-    </p>
-  </div>
+      <h1 className="mt-24 text-6xl tracking-widest text-poke-yellow">
+        Liste des pokémons
+      </h1>
+      <p className="mt-5 text-lg text-center text-poke-darkblue">
+        Votre pokédex personnel entièrement personnalisable.
+      </p>
+    </div>
   );
 }
-
-const App = () => {
-  return <page.current />;
-}
-
-export default App;
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter as BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { render } from "react-dom";
+import Home, { List } from "./App";
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/list" element={<List />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
